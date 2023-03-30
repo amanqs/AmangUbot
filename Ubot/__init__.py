f8eb58b4084d74da8e858cf9241dac3b1c2c93fc
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-30 07:21:46 +0700

    update __init__.py

--- Ubot/__init__.py
+++ Ubot/__init__.py
@@ -13,7 +13,22 @@ from pyrogram import Client, filters
 from pytgcalls import GroupCallFactory
 from Ubot.core import *
 
-from config import *
+from config import (
+    API_ID,
+    API_HASH,
+    BOT_TOKEN,
+    BOT_WORKERS,
+    SESSION1,
+    SESSION2,
+    SESSION3,
+    SESSION4,
+    SESSION5,
+    SESSION6,
+    SESSION7,
+    SESSION8,
+    SESSION9,
+    SESSION10,
+    )
 cmds = None
 CMD_HELP = {}
 clients = []
