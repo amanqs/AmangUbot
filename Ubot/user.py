aa72ff935c3f61fad68ad3ff86c7b0129dbde14a
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-30 07:11:21 +0700

    update user.py

--- Ubot/user.py
+++ Ubot/user.py
@@ -6,7 +6,8 @@ from pyrogram import (
     enums
 )
 from .logging import LOGGER
-from . import *
+from Ubot import API_ID, API_HASH, BOT_TOKEN, BOT_WORKERS
+from config import SESSION1, SESSION2, SESSION3, SESSION4, SESSION5, SESSION6, SESSION7, SESSION8, SESSION9, SESSION10
 
 
 class User1(Client):
