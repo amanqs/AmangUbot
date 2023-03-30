ac8fa46291ad40558f9e72641156c1569c25b3e4
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-30 08:33:08 +0700

    update __main__.py

--- Ubot/__main__.py
+++ Ubot/__main__.py
@@ -3,37 +3,38 @@ import time
 from datetime import datetime
 import asyncio
 from pyrogram import idle
-
+from pyrogram.errors import RPCError
 from uvloop import install
 from ubotlibs import *
 from Ubot import aiosession, bots, app, ids, LOOP
 from platform import python_version as py
-from .logging import LOGGER
+from Ubot.logging import LOGGER
 from pyrogram import __version__ as pyro
-from Ubot.core.db import *
-
 from Ubot.modules import ALL_MODULES
+from Ubot.core.db import *
 from config import SUPPORT, CHANNEL
 import os
 from dotenv import load_dotenv
+from pyrogram.errors import RPCError
+
+BOT_VER ="8.1.0"
 
 
 
 MSG_ON = """
-**Naya Premium Actived ✅**
+**New Ubot Actived ✅**
 ╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
 ◉ **Versi** : `{}`
 ◉ **Phython** : `{}`
 ◉ **Pyrogram** : `{}`
-**Ketik** `alive`
-**untuk Mengecheck Bot**
+**Ketik** `alive` **untuk Mengecheck Bot**
 ╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
 """
 
 
 async def main():
     await app.start()
-    LOGGER("Naya Premium").info("Memulai Ubot Pyro..")
+    LOGGER("Naya-Premium").info("Memulai..")
     for all_module in ALL_MODULES:
         importlib.import_module("Ubot.modules" + all_module)
     for bot in bots:
@@ -41,26 +42,27 @@ async def main():
             await bot.start()
             ex = await bot.get_me()
             user_id = ex.id
-            await join(bot)
             await buat_log(bot)
             botlog_chat_id = await get_botlog(user_id)
-            try:
-#            	await bot.send_message(botlog_chat_id, MSG_ON.format(BOT_VER, py, pyro))
-            except BaseException as a:
-                LOGGER("✓").warning(f"{a}")
-            LOGGER("✓").info("Startup Completed")
-            LOGGER("✓").info(f"Started as {ex.first_name} | {ex.id} ")
+            LOGGER("Naya-Premium").info("Startup Completed")
+            LOGGER("√").info(f"Started as {ex.first_name} | {ex.id} ")
+            await join(bot)
+            user = len(ids)
+#            await bot.send_message(botlog_chat_id, MSG_ON.format(BOT_VER, py(), pyro))
             ids.append(ex.id)
         except Exception as e:
             LOGGER("X").info(f"{e}")
+    
+#  await app.send_message(SUPPORT, MSG_BOT.format(py(), pyro, user))
     await idle()
     await aiosession.close()
+
     
 
 
               
 
 if __name__ == "__main__":
-    LOGGER("Naya Premium").info("Starting  Ubot")
+    LOGGER("Ubot").info("Starting  Ubot")
     install()
     LOOP.run_until_complete(main())
