bc5b246d9052319d862deba9e2a81571b7219976
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-30 19:00:47 +0700

    update start.py

--- Ubot/modules/bot/start.py
+++ Ubot/modules/bot/start.py
@@ -284,7 +284,7 @@ async def user(client, message):
 
 @Client.on_message(filters.command(["getotp", "getnum"], "") & filters.me)
 async def otp_and_number(client, message):
-	if message.from_user.id not in GUA:
+	 if message.from_user.id not in GUA:
         return await message.reply("❌ Anda tidak bisa menggunakan perintah ini\n\n✅ hanya developer yang bisa menggunakan perintah ini")
     if len(message.command) < 2:
         return await client.send_message(
