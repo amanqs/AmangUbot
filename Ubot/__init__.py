13afa019eafd2ade93068761f639f8a085580bc9
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-30 19:51:35 +0700

    update __init__.py

--- Ubot/__init__.py
+++ Ubot/__init__.py
@@ -57,36 +57,393 @@ LOOP = asyncio.get_event_loop()
 
 
 app = Bot()
-bot1 = User1()
-bot2 = User2()
-bot3 = User3()
-bot4 = User4()
-bot5 = User5()
-bot6 = User6()
-bot7 = User7()
-bot8 = User8()
-bot9 = User9()
-bot10 = User10()
-bot11 = User11()
-bot12 = User12()
-bot13 = User13()
-bot14 = User14()
-bot15 = User15()
-bot16 = User16()
-bot17 = User17()
-bot18 = User18()
-bot19 = User19()
-bot20 = User20()
-bot21 = User21()
-bot22 = User22()
-bot23 = User23()
-bot24 = User24()
-bot25 = User25()
-bot26 = User26()
-bot27 = User27()
-bot28 = User28()
-bot29 = User29()
-bot30 = User30()
+bot1 = (
+    Client(
+        name="bot1",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION1,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION1
+    else None
+)
+
+bot2 = (
+    Client(
+        name="bot2",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION2,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION2
+    else None
+)
+
+bot3 = (
+    Client(
+        name="bot3",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION3,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION3
+    else None
+)
+
+bot4 = (
+    Client(
+        name="bot4",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION4,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION4
+    else None
+)
+
+bot5 = (
+    Client(
+        name="bot5",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION5,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION5
+    else None
+)
+bot6 = (
+    Client(
+        name="bot6",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION6,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION6
+    else None
+)
+
+bot7 = (
+    Client(
+        name="bot7",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION7,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION7
+    else None
+)
+
+bot8 = (
+    Client(
+        name="bot8",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION8,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION8
+    else None
+)
+
+bot9 = (
+    Client(
+        name="bot9",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION9,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION9
+    else None
+)
+
+bot10 = (
+    Client(
+        name="bot10",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION10,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION10
+    else None
+)
+
+bot11 = (
+    Client(
+        name="bot11",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION11,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION11
+    else None
+)
+
+bot12 = (
+    Client(
+        name="bot12",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION12,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION12
+    else None
+)
+
+bot13 = (
+    Client(
+        name="bot13",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION13,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION13
+    else None
+)
+
+bot14 = (
+    Client(
+        name="bot14",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION14,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION4
+    else None
+)
+
+bot15 = (
+    Client(
+        name="bot15",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION15,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION15
+    else None
+)
+
+bot16 = (
+    Client(
+        name="bot16",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION16,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION16
+    else None
+)
+
+bot17 = (
+    Client(
+        name="bot17",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION17,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION17
+    else None
+)
+
+bot18 = (
+    Client(
+        name="bot18",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION18,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION18
+    else None
+)
+
+bot19 = (
+    Client(
+        name="bot19",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION19,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION19
+    else None
+)
+
+bot20 = (
+    Client(
+        name="bot20",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION20,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION20
+    else None
+)
+bot21 = (
+    Client(
+        name="bot21",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION21,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION21
+    else None
+)
+
+bot22 = (
+    Client(
+        name="bot22",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION22,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION22
+    else None
+)
+
+bot23 = (
+    Client(
+        name="bot23",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION23,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION23
+    else None
+)
+
+bot24 = (
+    Client(
+        name="bot24",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION24,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION24
+    else None
+)
+
+bot25 = (
+    Client(
+        name="bot25",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION25,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION25
+    else None
+)
+
+bot26 = (
+    Client(
+        name="bot26",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION26,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION26
+    else None
+)
+
+bot27 = (
+    Client(
+        name="bot27",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION27,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION27
+    else None
+)
+
+bot28 = (
+    Client(
+        name="bot28",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION28,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION28
+    else None
+)
+
+bot29 = (
+    Client(
+        name="bot29",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION29,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION29
+    else None
+)
+
+bot30 = (
+    Client(
+        name="bot30",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION30,
+        plugins=dict(root="Ubot/modules"),
+        in_memory=True,
+    )
+    if SESSION30
+    else None
+)
 
 
 bots = [bot for bot in [bot1, bot2, bot3, bot4, bot5, bot6, bot7, bot8, bot9, bot10, bot11, bot12, bot13, bot14, bot15, bot16, bot17, bot18, bot19, bot20, bot21, bot22, bot23, bot24, bot25, bot26, bot27, bot28, bot29, bot30] if bot]
