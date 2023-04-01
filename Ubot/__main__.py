d43edd4b12cf966d4ce1c0c0374512d5cb885665
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-04-01 14:24:22 +0700

    update __main__.py

--- Ubot/__main__.py
+++ Ubot/__main__.py
@@ -32,37 +32,38 @@ MSG_ON = """
 """
 
 
-async def main():
+async def start_bot():
     await app.start()
     LOGGER("Naya Premium").info("Memulai Ubot Pyro..")
     for all_module in ALL_MODULES:
         importlib.import_module("Ubot.modules" + all_module)
-    for bot in bots:
+    for cli in clients:
         try:
-            await bot.start()
-            ex = await bot.get_me()
+            await cli.start()
+            ex = await cli.get_me()
             user_id = ex.id
-            await buat_log(bot)
+            await buat_log(cli)
             botlog_chat_id = await get_botlog(user_id)
             LOGGER("Info").info("Startup Completed")
-            LOGGER("√").info(f"Started as {ex.first_name} | {ex.id} ")
-            await join(bot)
-            user = len(ids)
-            await bot.send_message(botlog_chat_id, MSG_ON.format(BOT_VER, py(), pyro))
+            LOGGER("√").info(f"Started as {ex.first_name}")
             ids.append(ex.id)
+            await join(cli)
+            await cli.send_message(botlog_chat_id, MSG_ON.format(BOT_VER, py(), pyro))
         except Exception as e:
             LOGGER("X").info(f"{e}")
-
     await idle()
+    install()
     await aiosession.close()
     await app.stop()
-
     
 
 
-              
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
 
+"""
 if __name__ == "__main__":
     LOGGER("Naya Premium").info("Starting  Ubot")
-    install()
+    
     LOOP.run_until_complete(main())
+"""
