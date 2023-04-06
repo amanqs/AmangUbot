6d53b8146b5f90908b946904b4a003f8b9350fe9
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-06 22:00:52 +0700

    update ping.py

--- Ubot/modules/basic/ping.py
+++ Ubot/modules/basic/ping.py
@@ -18,7 +18,7 @@ from datetime import datetime
 from . import *
 from ubotlibs.ubot.helper.PyroHelpers import *
 from Ubot import *
-from Ubot.core.db import set_prefix, get_prefix, prefix
+from Ubot.core.db import set_prefix, get_prefix
 from .systemstats import get_readable_time
 from ubotlibs.ubot.utils.tools import get_arg
 
