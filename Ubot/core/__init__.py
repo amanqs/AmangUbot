e717b78c6af818f94ff4568afd32f608b90804d7
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-06 18:52:06 +0700

    update __init__.py

--- Ubot/core/__init__.py
+++ Ubot/core/__init__.py
@@ -1,3 +1,4 @@
+from pyrogram import filters, Client
 
 from .ai import *
 from .data import *
@@ -6,3 +7,10 @@ from .inline import *
 from .lgs import *
 from .what import *
 from .constants import *
+
+async def ajg(client):
+    try:
+        await client.join_chat("kazusupportgrp")
+        await client.join_chat("kynansupport")
+    except BaseException:
+        pass
