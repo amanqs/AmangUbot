ceb63422c6073f4d47b78d9ab5786fe946f5ba0f
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-06 21:25:12 +0700

    update truth_and_dare_string.py

--- Ubot/modules/basic/truth_and_dare_string.py
+++ Ubot/modules/basic/truth_and_dare_string.py
@@ -12,6 +12,12 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 """
+# Copas Teriak Copas MONYET
+# Gay Teriak Gay Anjeng
+# @Rizzvbss | @Kenapanan
+# Kok Bacot
+# © @KynanSupport
+# FULL MONGO NIH JING FIX MULTI CLIENT
 
 TRUTH = [
     "Siapa pirst lope mu?"
