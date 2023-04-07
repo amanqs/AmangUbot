685fb590d748eb1c0a24c0e158591580ba33fabf
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-07 21:33:19 +0700

    update cos_cmd.py

--- Ubot/core/cos_cmd.py
+++ Ubot/core/cos_cmd.py
@@ -18,33 +18,35 @@ from pyrogram.errors.exceptions.bad_request_400 import MessageIdInvalid
 from .func import rm_markdown
 from Ubot import cmds, app
 
-
-
-
 class naya:
     @classmethod
     def on_cmd(
-        self,
+        cls,
         command: list,
     ):
         naya_filter = (filters.me & filters.command(command, cmds) & ~filters.via_bot)
-        def decorate_naya(func):
-            async def x_wrapper(client, message):
-                self.add_handler(x_wrapper, naya_filter)
-                return x_wrapper
-            return decorate_naya
+        return cls.decorate_naya(naya_filter)
 
     @classmethod
-    def on_cf(self, custom_filters):
+    def decorate_naya(cls, naya_filter):
+        def decorator(func):
+            async def x_wrapper(self, client, message):
+                self.add_handler(x_wrapper, naya_filter)
+                return await func(client, message)
+            return x_wrapper
+        return decorator
+
+    @classmethod
+    def on_cf(cls, custom_filters):
         def decorate_naya_cf(func):
             async def x_wrapper_cf(client, message):
                 self.add_handler(x_wrapper_cf, custom_filters)
                 return x_wrapper_cf
             return decorate_naya_cf
-    
+
     @classmethod
-    def add_handler(self, x_wrapper, nexaub_filter, cmd_grp):
+    def add_handler(cls, x_wrapper, naya_filter):
         app.add_handler(MessageHandler(x_wrapper, filters=naya_filter))
 
-    
-    
+
+
