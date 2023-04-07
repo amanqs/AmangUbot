d03f7542412a5e6d1a4e135115fb664461cccc91
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-07 21:40:17 +0700

    update ping.py

--- Ubot/modules/basic/ping.py
+++ Ubot/modules/basic/ping.py
@@ -130,9 +130,7 @@ async def pingme(client, message):
         )
     await ping_.delete()
   
-        
-
-@naya.on_cmd(command["pak"])
+@naya.on_cmd(command=["pak"])
 async def y(client, message):
     await message.reply("sesama gay itu monyet")
 
