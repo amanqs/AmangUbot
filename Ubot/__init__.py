bfed476aec33b266989dba4626a90e3f2c7174c4
amanqs <116822442+amanqs@users.noreply.github.com>
2023-04-13 00:33:07 +0700

    Update __init__.py

--- Ubot/__init__.py
+++ Ubot/__init__.py
@@ -279,7 +279,7 @@ bot15 = (
     else None
 )
   
-bots = [bot for bot in [bot1, bot2, bot3, bot4, bot5, bot6, bot7, bot8, bot9, bot10] if bot]
+bots = [bot for bot in [bot1, bot2, bot3, bot4, bot5, bot6, bot7, bot8, bot9, bot10, bot11, bot12, bot13, bot14, bot15] if bot]
 
 for bot in bots:
     if not hasattr(bot, "group_call"):
