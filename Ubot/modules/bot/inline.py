aecb3f92fd42b463ff4e286ee8562d678062a100
amanqs <116822442+amanqs@users.noreply.github.com>
2023-04-16 23:15:08 +0700

    Update inline.py

--- Ubot/modules/bot/inline.py
+++ Ubot/modules/bot/inline.py
@@ -87,10 +87,9 @@ async def alive_function(message, answers):
         f"<b>AmangUserbot</b>\n"
         f"<b> status: Premium {status} </b>\n"
         f"    <b> expired:</b> <code>{remaining_days}</code>\n"
-        f"    <b> user_id:</b> <code>{user}</code>\n"
         f"    <b> ping_dc:</b> <code>{ping} ms</code>\n"
-        f"    <b> users_count:</b> <code>{users} users</code>\n"
-        f"    <b> groups_count:</b> <code>{group} group</code>\n"
+        f"    <b> peer_ubot:</b> <code>{user}</code>\n"
+        f"    <b> peer_groups:</b> <code>{group} group</code>\n"
         f"    <b> uptime:</b> <code>{uptime}</code>\n")
     answers.append(
         InlineQueryResultArticle(
